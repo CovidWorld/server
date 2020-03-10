84130d8caa218819f8095cb1c461f0b19bfdb46f
dkozik <dkozik@sygic.com>
2020-03-10 13:24:13 +0100

    Add Create Device Function Draft

---
+++ Sygic.Corona.Profile/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Sygic.Corona.Profile;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace Sygic.Corona.Profile
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // inject your dependencies here
+        }
+    }
+}
