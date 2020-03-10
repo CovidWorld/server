db0262d3fbf077896a4a911c9cb6fe2936d569da
dkozik <dkozik@sygic.com>
2020-03-10 21:22:48 +0100

    Add application projects

---
+++ Sygic.Corona.Application/Commands/CreateProfileCommand.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Sygic.Corona.Contracts.Responses;
+
+namespace Sygic.Corona.Application.Commands
+{
+    public class CreateProfileCommand : IRequest<CreateProfileResponse>
+    {
+        public string DeviceId { get; }
+        public string PushToken { get; }
+        public string Locale { get; }
+        public double? Latitude { get; }
+        public double? Longitude { get; }
+        public int? Accuracy { get; }
+        public string AuthToken { get; }
+
+        public CreateProfileCommand(string deviceId, string pushToken, string locale, double? latitude, double? longitude, int? accuracy, string authToken)
+        {
+            DeviceId = deviceId;
+            PushToken = pushToken;
+            Locale = locale;
+            Latitude = latitude;
+            Longitude = longitude;
+            Accuracy = accuracy;
+            AuthToken = authToken;
+        }
+    }
+}
---
+++ Sygic.Corona.Application/Commands/CreateProfileCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Sygic.Corona.Contracts.Responses;
+using Sygic.Corona.Domain;
+
+namespace Sygic.Corona.Application.Commands
+{
+    public class CreateProfileCommandHandler : IRequestHandler<CreateProfileCommand, CreateProfileResponse>
+    {
+        private readonly IRepository repository;
+
+        public CreateProfileCommandHandler(IRepository repository)
+        {
+            this.repository = repository;
+        }
+        public async Task<CreateProfileResponse> Handle(CreateProfileCommand request, CancellationToken cancellationToken)
+        {
+            long lastId = await repository.GetLastId(cancellationToken);
+            long nextId = lastId + 1;
+            var location = new Location(request.Latitude, request.Longitude, request.Accuracy);
+            var profile = new Profile(nextId, request.DeviceId, request.PushToken, request.Locale, location, request.AuthToken);
+
+            await repository.CreateProfileAsync(profile, cancellationToken);
+            await repository.UnitOfWork.SaveChangesAsync(cancellationToken);
+
+            return new CreateProfileResponse{ Id = nextId };
+
+        }
+    }
+}
