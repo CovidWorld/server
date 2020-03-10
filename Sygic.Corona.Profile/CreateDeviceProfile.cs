1db8be2d70322c8cec0568fa6cef272e5eb41ef6
dkozik <dkozik@sygic.com>
2020-03-10 21:23:19 +0100

    Add function api project

--- Sygic.Corona.Profile/CreateDeviceProfile.cs
+++ Sygic.Corona.Profile/CreateDeviceProfile.cs
@@ -1,39 +1,41 @@
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Sygic.Corona.Application.Commands;
+using Sygic.Corona.Contracts.Requests;
 
 namespace Sygic.Corona.Profile
 {
     public class CreateDeviceProfile
     {
-        public CreateDeviceProfile()
+        private readonly IMediator mediator;
+
+        public CreateDeviceProfile(IMediator mediator)
         {
-            
+            this.mediator = mediator;
         }
 
         [FunctionName("CreateDeviceProfile")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log, CancellationToken cancellationToken)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var data = JsonConvert.DeserializeObject<CreateProfileRequest>(requestBody);
+            
+            var command = new CreateProfileCommand(data.DeviceId, data.PushToken, data.Locale, data.Latitude, data.Longitude, data.Accuracy, "");
+            var result = await mediator.Send(command, cancellationToken);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(result);
         }
     }
 }
--- Sygic.Corona.Profile/Startup.cs
+++ Sygic.Corona.Profile/Startup.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using System;
+using System.Reflection;
+using MediatR;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Sygic.Corona.Application.Commands;
+using Sygic.Corona.Domain;
+using Sygic.Corona.Infrastructure;
+using Sygic.Corona.Infrastructure.Repositories;
 using Sygic.Corona.Profile;
 
 [assembly: FunctionsStartup(typeof(Startup))]
@@ -9,6 +18,13 @@ namespace Sygic.Corona.Profile
         public override void Configure(IFunctionsHostBuilder builder)
         {
             // inject your dependencies here
+            builder.Services.AddDbContext<CoronaContext>(o => o.UseCosmos(
+                Environment.GetEnvironmentVariable("CosmosEndpoint"),
+                Environment.GetEnvironmentVariable("CosmosAuthKey"),
+                Environment.GetEnvironmentVariable("CosmosDatabase")));
+
+            builder.Services.AddScoped<IRepository, CoronaRepository>();
+            builder.Services.AddMediatR(typeof(CreateProfileCommand).GetTypeInfo().Assembly);
         }
     }
 }
