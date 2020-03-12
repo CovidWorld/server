17b5b2eaf72c02041e838c52157edbe4445b2d42
dkozik <dkozik@sygic.com>
2020-03-12 11:17:00 +0100

    Make push notification token required

--- Sygic.Corona.Application/Validations/CreateProfileCommandValidator.cs
+++ Sygic.Corona.Application/Validations/CreateProfileCommandValidator.cs
@@ -9,6 +9,7 @@ namespace Sygic.Corona.Application.Validations
         {
             RuleFor(x => x.Locale).NotEmpty().NotNull().WithMessage("Provide language for profile (locale).");
             RuleFor(x => x.DeviceId).NotEmpty().NotNull().WithMessage("Provide Device ID.");
+            RuleFor(x => x.PushToken).NotEmpty().NotNull().WithMessage("Provide push notification registration token.");
         }
     }
 }
--- Sygic.Corona.Infrastructure/Configurations/ProfileConfiguration.cs
+++ Sygic.Corona.Infrastructure/Configurations/ProfileConfiguration.cs
@@ -11,6 +11,7 @@ namespace Sygic.Corona.Infrastructure.Configurations
             builder.HasKey(x => x.Id);
             builder.Property(x => x.DeviceId).IsRequired();
             builder.Property(x => x.Locale).IsRequired();
+            builder.Property(x => x.PushToken).IsRequired();
             builder.OwnsOne(x => x.Location, n => { n.WithOwner(); });
             
             var navigation = builder.Metadata.FindNavigation(nameof(Profile.Contacts));
