c318d52c21b48632a9389720944f7934dca0505c
dkozik <dkozik@sygic.com>
2020-03-23 15:17:19 +0100

    Change morning alert notification message.

--- Sygic.Corona.Application/Commands/SendMorningAlertNotificationCommandHandler.cs
+++ Sygic.Corona.Application/Commands/SendMorningAlertNotificationCommandHandler.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Newtonsoft.Json;
 using Sygic.Corona.Infrastructure.Services.CloudMessaging;
 
 namespace Sygic.Corona.Application.Commands
@@ -14,6 +15,7 @@ namespace Sygic.Corona.Application.Commands
         {
             this.messagingService = messagingService;
         }
+
         protected override async Task Handle(SendMorningAlertNotificationCommand request, CancellationToken cancellationToken)
         {
             if (!string.IsNullOrEmpty(request.ProfileInQuarantine.PushToken))
@@ -22,10 +24,47 @@ namespace Sygic.Corona.Application.Commands
                 if (daysLeft.HasValue)
                 {
                     string days = daysLeft.Value.Days.ToString();
-                    await messagingService.SendMessageToDevice(request.ProfileInQuarantine.PushToken, new { type = "MORNING_QUARANTINE_ALERT", daysLeft = days }, cancellationToken);
+                    string messageText = 
+                        $"Váš zostávajúci počet dní v karanténe: {days}. Ďakujeme že dodržiavate domácu karanténu. Správajme sa zodpovedne voči blízkym a nášmu okoliu. MV SR";
+                    var message = new Notification
+                    {
+                        Priority = "high",
+                        Data = new NotificationData
+                        {
+                            Type = "MORNING_QUARANTINE_ALERT"
+                        },
+                        NotificationContent = new NotificationContent
+                        {
+                            Title = "",
+                            Body = messageText,
+                            Sound = "default"
+                        },
+                        ContentAvailable = true
+                    };
+                    await messagingService.SendMessageToDevice(request.ProfileInQuarantine.PushToken, message, cancellationToken);
                 }
                 
             }
+        }
+
+        private class Notification
+        {
+            [JsonProperty("priority")] public string Priority { get; set; }
+            [JsonProperty("content-available")] public bool ContentAvailable { get; set; }
+            [JsonProperty("data")] public NotificationData Data { get; set; }
+            [JsonProperty("notification")] public NotificationContent NotificationContent { get; set; }
+        }
+
+        private class NotificationData
+        {
+            [JsonProperty("type")] public string Type { get; set; }
+        }
+
+        private class NotificationContent
+        {
+            [JsonProperty("title")] public string Title { get; set; }
+            [JsonProperty("body")] public string Body { get; set; }
+            [JsonProperty("sound")] public string Sound { get; set; }
         }
     }
 }
