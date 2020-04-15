04db68fad47dd4fec4dd5e18791dc0320637fd9c
dkozik <dkozik@sygic.com>
2020-04-15 08:53:44 +0200

    Send notification to user after quarantine is updated.

--- Sygic.Corona.Application/Commands/UpdateQuarantineCommandHandler.cs
+++ Sygic.Corona.Application/Commands/UpdateQuarantineCommandHandler.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Sygic.Corona.Domain;
+using Sygic.Corona.Infrastructure.Services.CloudMessaging;
 
 namespace Sygic.Corona.Application.Commands
 {
     public class UpdateQuarantineCommandHandler : AsyncRequestHandler<UpdateQuarantineCommand>
     {
         private readonly IRepository repository;
+        private readonly IMediator mediator;
 
-        public UpdateQuarantineCommandHandler(IRepository repository)
+        public UpdateQuarantineCommandHandler(IRepository repository, IMediator mediator)
         {
             this.repository = repository;
+            this.mediator = mediator;
         }
         
         protected override async Task Handle(UpdateQuarantineCommand request, CancellationToken cancellationToken)
@@ -21,11 +23,31 @@ namespace Sygic.Corona.Application.Commands
             var profiles = await repository.GetProfilesByCovidPassAsync(request.CovidPass, cancellationToken);
 
             var profileList = profiles.ToList();
+
+            var message = new Notification
+            {
+                //Priority = "high",
+                //Data = new NotificationData
+                //{
+                //    Type = "UPDATE_QUARANTINE_ALERT"
+                //},
+                NotificationContent = new NotificationContent
+                {
+                    Title = "Covid19 ZostanZdravy",
+                    Body = "Doba vasej karanteny sa zmenila, viac informacii najdete v aplikacii",
+                    Sound = "default"
+                },
+                //ContentAvailable = true
+            };
+
+
             if (profileList.Any())
             {
                 foreach (var profile in profileList)
                 {
                     profile.UpdateQuarantine(request.QuarantineStart, request.QuarantineEnd);
+                    var command = new SendPushNotificationCommand(profile.Id, message);
+                    await mediator.Send(command, cancellationToken);
                 }
             }
 
