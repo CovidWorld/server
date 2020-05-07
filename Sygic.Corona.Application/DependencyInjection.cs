156a3ae62b6b6d6f1bc5330f5370e57ec69a75be
dkozik <dkozik@sygic.com>
2020-05-07 11:32:11 +0200

    Add dependency injection extensions. TODO: integration

---
+++ Sygic.Corona.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Sygic.Corona.Application.Behaviors;
+using Sygic.Corona.Application.Validations;
+
+namespace Sygic.Corona.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidatorBehavior<,>));
+            services.AddScoped<ValidationProcessor>();
+
+            return services;
+        }
+    }
+}
