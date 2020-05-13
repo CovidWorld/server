36c069237f607da65f246c211477ff1a14f02e14
Daniel Kožík <kozikdaniel@gmail.com>
2020-05-13 16:25:38 +0200

    Add separate funcion API draft for quarantine app

---
+++ Sygic.Corona.QuarantineApi/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Sygic.Corona.Application;
+using Sygic.Corona.Infrastructure;
+using Sygic.Corona.QuarantineApi;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace Sygic.Corona.QuarantineApi
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            builder.Services.AddApplication()
+                .AddInfrastructure(config);
+        }
+    }
+}
