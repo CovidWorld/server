683dfee1f15045123bf68f3f74ebbb9625e4ef97
Daniel Kožík <kozikdaniel@gmail.com>
2020-05-15 11:29:59 +0200

    Change authorization for update quarantine

--- Sygic.Corona.QuarantineApi/UpdateQuarantine.cs
+++ Sygic.Corona.QuarantineApi/UpdateQuarantine.cs
@@ -1,5 +1,4 @@
 ﻿using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
@@ -14,44 +13,45 @@ using Sygic.Corona.Application.Validations;
 using Sygic.Corona.Contracts.Requests;
 using Sygic.Corona.Domain;
 using Sygic.Corona.Domain.Common;
-using Sygic.Corona.Infrastructure.Services.Authorization;
+using System.Web.Http;
+using System;
 
 namespace Sygic.Corona.QuarantineApi
 {
     public class UpdateQuarantine
     {
-        private readonly ISignVerification verification;
         private readonly IMediator mediator;
         private readonly ValidationProcessor validation;
 
-        public UpdateQuarantine(ISignVerification verification, IMediator mediator, ValidationProcessor validation)
+        public UpdateQuarantine(IMediator mediator, ValidationProcessor validation)
         {
-            this.verification = verification;
             this.mediator = mediator;
             this.validation = validation;
         }
 
         [FunctionName("UpdateQuarantine")]
         public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "quarantine")]
-            HttpRequest req,
-            ILogger log, CancellationToken cancellationToken)
+            HttpRequest req, ILogger log, CancellationToken cancellationToken)
         {
             try
             {
-                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-                string[] signatureHeaderParameters = req.Headers["X-Signature"].ToString().Split(':');
-                if (signatureHeaderParameters.Length > 2)
+                var qs = req.Query;
+
+                if (!qs.ContainsKey("apiKey"))
                 {
-                    return new BadRequestResult();
+                    return new BadRequestErrorMessageResult("Missing query param: apiKey");
                 }
 
-                var isVerified = verification.Verify(requestBody, signatureHeaderParameters.First(), signatureHeaderParameters.Last());
+                var apiKey = qs["apiKey"];
+                bool isAuthorized = apiKey == Environment.GetEnvironmentVariable("NcziApiKey");
 
-                if (!isVerified)
+                if (!isAuthorized)
                 {
+                    log.LogWarning("Unauthorized call.");
                     return new UnauthorizedResult();
                 }
 
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
                 var data = JsonConvert.DeserializeObject<UpdateQuarantineRequest>(requestBody);
                 var command = new UpdateQuarantineCommand(data.CovidPass, data.QuarantineStart, data.QuarantineEnd, data.BorderCrossedAt,
                     new Address(
