3f320bed28f38a8163edddb4fcd3df1815752e08
Daniel Kožík <kozikdaniel@gmail.com>
2020-05-18 18:31:20 +0200

    Fix area exit create failed presence check

--- Sygic.Corona.Application/Commands/NotifyAreaExitCommandHandler.cs
+++ Sygic.Corona.Application/Commands/NotifyAreaExitCommandHandler.cs
@@ -11,18 +11,17 @@ namespace Sygic.Corona.Application.Commands
     public class NotifyAreaExitCommandHandler : AsyncRequestHandler<NotifyAreaExitCommand>
     {
         private readonly IRepository repository;
-        private readonly IMediator mediator;
         private readonly IDateTimeConvertService timeConvertService;
 
-        public NotifyAreaExitCommandHandler(IRepository repository, IMediator mediator, IDateTimeConvertService timeConvertService)
+        public NotifyAreaExitCommandHandler(IRepository repository, IDateTimeConvertService timeConvertService)
         {
             this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
-            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
             this.timeConvertService = timeConvertService ?? throw new ArgumentNullException(nameof(timeConvertService));
         }
 
         protected override async Task Handle(NotifyAreaExitCommand request, CancellationToken cancellationToken)
         {
+            var now = DateTime.UtcNow;
             var profile = await repository.GetProfileAsync(request.ProfileId, request.DeviceId, cancellationToken);
             if (profile == null)
             {
@@ -36,10 +35,11 @@ namespace Sygic.Corona.Application.Commands
 
             var exit = new AreaExit(profile.Id, request.Severity, timeConvertService.UnixTimeStampToDateTime(request.RecordTimestamp));
             profile.AddAreaExit(exit);
-            await repository.UnitOfWork.SaveChangesAsync(cancellationToken);
 
-            var createPresenceCheckCommand = new CreatePresenceCheckCommand(profile.CovidPass, new TimeSpan(0), PresenceCheckStatus.LEFT);
-            await mediator.Send(createPresenceCheckCommand, cancellationToken);
+            var check = new PresenceCheck(profile.Id, now, now, PresenceCheckStatus.LEFT);
+            profile.AddPresenceCheck(check);
+
+            await repository.UnitOfWork.SaveChangesAsync(cancellationToken);
         }
     }
 }
